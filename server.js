8803c62c94a429517723434a3a5ec13cf3683aec
abrisc05 <152277959+abrisc05@users.noreply.github.com>
2024-04-14 22:32:40 -0400

    .env changes

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const pool = new Pool ({
    user: 'postgres',
-   host: 'localhost',
+   host: 'postgres.cvmwieseujq4.us-east-2.rds.amazonaws.com',
    database: 'postgres',
-   password: '296502',
+   password: 'postgres',
    port: 5432,
 })
 
